424085ae720ad0288e1d7d747e8f54dc39e4f80b
getingshuai123 <162959073+getingshuai123@users.noreply.github.com>
2025-11-04 17:07:01 +0900

    Update SwaggerConfig.java

--- backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
+++ backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
                         .url("https://github.com/tianyf925"))
                 )
                 .servers(java.util.Arrays.asList(
-                        new Server().url("http://51.20.85.217:8080"),
+                        new Server().url("http://13.51.197.180:8080"),
                         new Server().url("http://api.airline.com")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
